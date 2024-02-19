e1bde0bb66b0060a956de38dcf77d596f507b889
cassandro-pereira <112014056+cassandro-pereira@users.noreply.github.com>
2024-02-19 10:48:23 -0300

    Update encrypt-text.py

--- encrypt-text.py
+++ encrypt-text.py
@@ -2,14 +2,20 @@
 from Crypto.PublicKey import RSA
 
 
-secret_code = "Unguessable"
+secret_code = "Secret_to_all"
 key = RSA.generate(2048)
+#encrypted_key is the private key
 encrypted_key = key.export_key(passphrase=secret_code, pkcs=8,
                               protection="scryptAndAES128-CBC",
                               prot_params={'iteration_count':131072})
 
 with open("rsa_key.bin", "wb") as f:
-    f.write(encrypted_key)
+    f.write(encrypted_key)    
 
 print(key.publickey().export_key())
 print(key.publickey().__doc__)
+#print(encrypted_key)
+
+#print encripted file 
+fread = open("rsa_key.bin", "r")
+print(fread.read())
